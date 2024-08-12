a40f823d20ccf46979130faf4f7804d2c9dfb89a
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-08-12 20:55:55 -0300

    inserção de imports no app.js, alterações index.js, criação da pasta signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
+import DefaultLayout from './components/DefaultLayout';
+import SimpleLayout from './components/SimpleLayout';
 
 function App(){
     return(
         <>
-        <Header/>
         <BrowserRouter>
             <Routes>
-                <Route path="/" Component={Home}/>
-                <Route path="/login" Component={Login}/>
+                <Route path="/" element={<DefaultLayout><Home/></DefaultLayout>}/>
+                <Route path="/login" element={<SimpleLayout><Login/></SimpleLayout>}/>
             </Routes>
         </BrowserRouter>
-        <Footer/>
         </>
     );
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Login.css";
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
 
 const Login = () => {
     return (
         <div>
-            <h1>Página de Login</h1>
+           <Card title="Login">
+                <InputText/>
+                <Password feedback={false}/>
+                <Button label="Login"/>
+           </Card>
+           <div class="grid">
+             <div class="col-12 md:col-6 lg:col-3">
+                <div class="text-center p-3 border-round-sm bg-primary font-bold">col-12 md:col-6 lg:col-3</div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+                <div class="text-center p-3 border-round-sm bg-primary font-bold">col-12 md:col-6 lg:col-3</div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+                <div class="text-center p-3 border-round-sm bg-primary font-bold">col-12 md:col-6 lg:col-3</div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+                <div class="text-center p-3 border-round-sm bg-primary font-bold">col-12 md:col-6 lg:col-3</div>
+            </div>
+            </div>
         </div>
-    )
+    );
 }
 export default Login;
