ee4af7ddd1bf47e354f65c881e9207d28f19b3e6
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-08-17 15:46:58 -0300

    Commit de checkpoint. Alteração na tela de cadastro, botão cancelar leva à tela de login (por enquanto)

--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -5,6 +5,7 @@ import { InputText } from 'primereact/inputtext';
 import { FloatLabel } from 'primereact/floatlabel';
 import { Helmet } from 'react-helmet';
 import { Button } from 'primereact/button';
+import { Password } from 'primereact/password';
 
 const Signup = () => {
     const[value, setValue] = useState('');
@@ -14,21 +15,15 @@ const Signup = () => {
             <Helmet>
                 <title>Cadastro</title>
             </Helmet>
-            <Card title="Cadastro" subTitle="Insira seus dados para criar uma conta" footer={""} header={""} className="md:w-30rem card">
-                <div className="card flex justify-content-center">
-                    <FloatLabel>
-                        <InputText id="nome" value={value} onChange={(e) => setValue(e.target.value)} />
-                        <label for="nome">Nome</label>
-                    </FloatLabel>
-                </div>
-                <div className="card flex justify-content-center">
-                    <FloatLabel>
-                        <InputText id="nome" value={value} onChange={(e) => setValue(e.target.value)} />
-                        <label for="nome">E-mail</label>
-                    </FloatLabel>
-                </div>
-                <Button label="Cancelar" className="bg-red-400 border-red-400" />
-                <Button label="Cadastrar" className="bg-green-400 border-green-400" />
+            <Card title="Cadastro" subTitle="Insira seus dados para criar uma conta" footer={""} header={""} className="md:w-30rem signupcard">
+                <label htmlFor="username">Nome Completo </label>
+                <InputText inputStyle={{ width: '100%' }} className="w-full mb-3" aria-describedby="username-help" required/>
+                <label htmlFor="username">E-mail </label>
+                <InputText inputStyle={{ width: '100%' }} className="w-full mb-3" aria-describedby="username-help" required/>
+                <label htmlFor="password">Senha </label>
+                <Password inputStyle={{ width: '100%' }} className="w-full mb-3" value={value} onChange={(e) => setValue(e.target.value)} toggleMask required/>
+                <a href="/login"><Button label="Cancelar" className="bg-red-400 border-red-400 m-2" /></a>
+                <Button label="Cadastrar" className="bg-green-400 border-green-400 m-2" />
             </Card>
                 
         </div>
