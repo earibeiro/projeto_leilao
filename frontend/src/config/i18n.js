bf435a6b6ea09025f2a88d3099f8330265e83d1e
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-08-26 20:12:04 -0300

    com este commit foi iniciada a adição da internacionalização, será dada continuidade ao longo da aula

---
+++ frontend/src/config/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const resources = {
+  en: {
+    translation: {
+      welcome: "Welcome",
+    },
+  },
+  pt: {
+    translation: {
+      welcome: "Bem-vindo",
+    },
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,16 +1,18 @@
 import React, { useRef } from "react";
 import "./Home.css";
 import { Helmet } from 'react-helmet';
+import { useTranslation } from "react-i18next";
 
 
 const Home = () => {
+    const {t} = useTranslation();
 
     return (
         <div>
             <Helmet>
                 <title>Página Inicial</title>
             </Helmet>
-            <h1>Página Inicial</h1>
+            <h1>Seja {t('welcome')} Página Inicial</h1>
         </div>
     );
 }
